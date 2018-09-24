cafcfae4e9f3cb6b69fc2a8b0180fd2ffe5671c5
Andrew Rossi <aroccorossi@gmail.com>
2018-09-24 11:13:16 -0400

    getting layout colors situated

--- src/ui/base/color/color.js
+++ src/ui/base/color/color.js
@@ -1,5 +1,13 @@
 const colors = {
+  black: '#000000',
+  gray85: '#2F302F',
+  gray70: '#999999',
+  gray40: '#D8D8D8',
+  gray30: '#E4E4E4',
+  white: '#FFFFFF',
 
+  blue: '#0201FE',
+  violet: '#E4006A',
 };
 
 export default colors;
