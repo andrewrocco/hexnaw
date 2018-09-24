d8df5d35477b52a89f4638a35c6db16ab46e8675
Andrew Rossi <aroccorossi@gmail.com>
2018-09-24 11:17:30 -0400

    adding basic button colors for now

--- src/ui/base/color/colorVariables.js
+++ src/ui/base/color/colorVariables.js
@@ -1,1 +1,8 @@
 import colors from './colors';
+
+const colorVariables = {
+  buttonPrimaryBackground: colors.blue,
+  buttonPrimaryBackgroundHover: colors.blue,
+};
+
+export default colorVariables;
