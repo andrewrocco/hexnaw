34e8eacf9e2d9e9a89d9957f1c640f4343927997
Andrew Rossi <aroccorossi@gmail.com>
2018-10-03 15:18:06 -0400

    remove test for now

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import colorTesting from '../src/utils/colorTesting';
 
-const Index = () => {
-  const test = colorTesting();
-
-  return (
-    <div>Hex Naw!</div>
-  );
-};
+const Index = () => (
+  <div>Hex Naw!</div>
+);
 
 export default Index;
