bd305dc7ec80c42b2fab7fa8976e790f699f5c36
Andrew Rossi <aroccorossi@gmail.com>
2018-10-03 15:04:45 -0400

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import colorTesting from '../src/utils/colorTesting';
 
 const Index = () => {
   const test = colorTesting();
-  console.log(test);
 
   return (
     <div>Hex Naw!</div>
@@ -11,5 +10,3 @@ const Index = () => {
 };
 
 export default Index;
-
-// export default () => <div>Welcome to next.js</div>;
