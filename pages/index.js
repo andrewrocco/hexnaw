447f0460eeb7f169360c4364c9f942d17a898cb2
Andrew Rossi <aroccorossi@gmail.com>
2018-10-04 12:25:15 -0400

    add Head component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import React, { Fragment } from 'react';
+import Head from 'next/head';
+
 import { ColorForm } from '../src/components/ColorForm';
 
 const Index = () => (
   <Fragment>
+    <Head>
+      <title>Hex Naw | A color accessibility tool for designers and developers.</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
     <div>Hex Naw!</div>
     <ColorForm />
   </Fragment>
