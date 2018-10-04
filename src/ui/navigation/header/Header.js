6cfb24fbd1345122f04104e874e5e6da41a7bcd0
Andrew Rossi <aroccorossi@gmail.com>
2018-10-04 16:49:13 -0400

    added flexbox implementation

--- src/ui/navigation/header/Header.js
+++ src/ui/navigation/header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Box, Flex } from '@rebass/grid';
 
 import { HeaderLink } from '../header_link/HeaderLink';
 
 export const Header = () => (
-  <div>
-    <HeaderLink href="/">Hex Naw</HeaderLink>
-    <HeaderLink href="/about" prefetch>About</HeaderLink>
-    <HeaderLink href="https://thescenery.co" prefetch>The Scenery</HeaderLink>
-  </div>
+  <Flex>
+    <Box mr="auto">Hello</Box>
+    <Box alignSelf="flex-end">
+      <HeaderLink href="/">Hex Naw</HeaderLink>
+      <HeaderLink href="/about" prefetch>About</HeaderLink>
+      <HeaderLink href="https://thescenery.co" prefetch>The Scenery</HeaderLink>
+    </Box>
+  </Flex>
 );
