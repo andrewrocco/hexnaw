0dd6c586304ea64add4d2183a06bd796bfdbc828
Andrew Rossi <aroccorossi@gmail.com>
2018-10-04 14:21:36 -0400

    create AppHead component

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 
+import { AppHead } from '../src/components/layout/AppHead';
 import { Header } from '../src/components/layout/Header';
 
 const About = () => (
   <Fragment>
+    <AppHead pageTitle="About" />
     <Header />
     <div>The About Page</div>
   </Fragment>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
 import React, { Fragment } from 'react';
-import Head from 'next/head';
 
+import { AppHead } from '../src/components/layout/AppHead';
 import { Header } from '../src/components/layout/Header';
 import { ColorForm } from '../src/components/report/ColorForm';
 
 const Index = () => (
   <Fragment>
-    <Head>
-      <title>Hex Naw | A color accessibility tool for designers and developers.</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Head>
+    <AppHead />
     <Header />
     <div>Hex Naw!</div>
     <ColorForm />
