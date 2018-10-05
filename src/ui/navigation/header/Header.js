f8e18b43112d2a8e1a8e26796ee032d660217898
Andrew Rossi <aroccorossi@gmail.com>
2018-10-05 14:42:22 -0400

    working on Header state and mobile nav

--- src/ui/navigation/header/Header.js
+++ src/ui/navigation/header/Header.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import { Box, Flex } from '@rebass/grid';
+import React, { PureComponent } from 'react';
+import { Box } from '@rebass/grid';
+import classnames from 'classnames';
 
 import { HeaderLink } from '../header_link';
 import { HeaderLogo } from '../header_logo';
-import { StyledNav } from './Header.styles';
+import { StyledHeader, StyledNav, StyledNavButton } from './Header.styles';
 
-export const Header = () => (
-  <Flex alignItems="center">
-    <Box mr="auto">
-      <HeaderLogo />
-    </Box>
-    <Box>
-      <StyledNav>
-        <HeaderLink href="/">Hex Naw</HeaderLink>
-        <HeaderLink href="/about" prefetch>About</HeaderLink>
-        <HeaderLink href="https://thescenery.co" prefetch>The Scenery &rarr;</HeaderLink>
-      </StyledNav>
-    </Box>
-  </Flex>
-);
+export class Header extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menuOpen: false,
+    };
+
+    this.handleMenuToggle = this.handleMenuToggle.bind(this);
+  }
+
+  handleMenuToggle() {
+    const { menuOpen } = this.state;
+    this.setState({ menuOpen: !menuOpen });
+  }
+
+  render() {
+    const { menuOpen } = this.state;
+
+    return (
+      <StyledHeader alignItems="center">
+        <Box mr="auto">
+          <HeaderLogo />
+        </Box>
+        <Box>
+          <StyledNav open={menuOpen}>
+            <HeaderLink href="/">Hex Naw</HeaderLink>
+            <HeaderLink href="/about" prefetch>About</HeaderLink>
+            <HeaderLink href="https://thescenery.co" prefetch>The Scenery &rarr;</HeaderLink>
+          </StyledNav>
+          <StyledNavButton onClick={this.handleMenuToggle}>Click Me</StyledNavButton>
+        </Box>
+      </StyledHeader>
+    );
+  }
+}
--- src/ui/navigation/header/Header.styles.js
+++ src/ui/navigation/header/Header.styles.js
@@ -1,5 +1,24 @@
 import styled from 'styled-components';
+import { Flex } from '@rebass/grid';
+
+export const StyledHeader = styled(Flex)``;
 
 export const StyledNav = styled.nav`
   padding: 21px 22px;
+
+  @media only screen and (max-width: 700px) {
+    box-sizing: content-box;
+    left: 50%;
+    position: fixed;
+    text-align: center;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    transition: all .05s ease-in-out;
+    width: 100%;
+    z-index: 500;
+  }
+`;
+
+export const StyledNavButton = styled.button`
+  display: none;
 `;
