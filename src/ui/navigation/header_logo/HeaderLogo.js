74801cf4c91309518f9dd782970c67fbd800c630
Andrew Rossi <aroccorossi@gmail.com>
2018-10-05 14:00:35 -0400

    move styles into new file for HeaderLogo

--- src/ui/navigation/header_logo/HeaderLogo.js
+++ src/ui/navigation/header_logo/HeaderLogo.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
-import styled from 'styled-components';
 
 import colorVariables from '../../base/color/colorVariables';
-
-const StyledLink = styled.a`
-  background: ${colorVariables.headerLogoBackground};
-  float: left;
-  font-size: 20px;
-  padding: 22px;
-  text-decoration: none;
-`;
-
-const StyledSVG = styled.svg`
-  float: left;
-  height: 18px;
-  width: 166px;
-`;
+import { StyledLink, StyledSVG } from './HeaderLogo.styles';
 
 export const HeaderLogo = () => (
   <Link href="/" passHref>
