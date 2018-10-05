6ddbc14c48636bc2aecfa22eb718177aed3cf3cf
jonmcgill <jon.d.mcgill@gmail.com>
2018-10-05 11:41:33 -0400

    add logic for validation, add button, submit button

--- src/components/report/ColorForm.js
+++ src/components/report/ColorForm.js
@@ -13,11 +13,23 @@ export class ColorForm extends PureComponent {
       colorCount: 0,
       colorResults: {},
       hexInputNames: Object.keys(this.initialValues),
+      isAddButtonDisabled: true,
+      isSubmitButtonDisabled: true,
     };
 
     this.handleResetProxy = null;
     this.addHexInput = this.addHexInput.bind(this);
     this.resetForm = this.resetForm.bind(this);
+    this.handleValidation = this.handleValidation.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { hexInputNames } = this.state;
+
+    if (prevState.hexInputNames.length < hexInputNames.length) {
+      document.querySelector(`[name="hex${hexInputNames.length - 1}"]`).focus();
+    }
   }
 
   addHexInput(e) {
@@ -27,30 +39,57 @@ export class ColorForm extends PureComponent {
         ...hexInputNames,
         `hex${hexInputNames.length}`,
       ],
+      isAddButtonDisabled: true,
+      isSubmitButtonDisabled: true,
     }));
   }
 
   resetForm() {
-    this.setState({ hexInputNames: Object.keys(this.initialValues) });
+    this.setState({
+      colorCount: 0,
+      hexInputNames: Object.keys(this.initialValues),
+      isAddButtonDisabled: true,
+      isSubmitButtonDisabled: true,
+    });
+
     this.handleResetProxy();
   }
 
+  handleValidation(values) {
+    const { hexInputNames } = this.state;
+    const validHexValues = Object.keys(values).filter(hexID => (
+      (/^[a-fA-F0-9]{6}$|^[a-fA-F0-9]{3}$/).test(values[hexID])
+    ));
+    const hasInvalid = validHexValues.length !== hexInputNames.length;
+    const notEnough = validHexValues.length < 2;
+
+    this.setState({
+      colorCount: validHexValues.length,
+      isAddButtonDisabled: hasInvalid,
+      isSubmitButtonDisabled: hasInvalid || notEnough,
+    });
+  }
+
+  handleSubmit(values, actions) {
+    actions.setSubmitting(false);
+    this.setState({ colorResults: testColors(values) });
+  }
+
   render() {
-    const { colorCount, colorResults, hexInputNames } = this.state;
+    const {
+      colorCount,
+      colorResults,
+      hexInputNames,
+      isAddButtonDisabled,
+      isSubmitButtonDisabled,
+    } = this.state;
 
     return (
       <div>
         <Formik
           initialValues={this.initialValues}
-          validate={(values) => {
-            console.log(values);
-          }}
-          onSubmit={(values, actions) => {
-            console.log(actions);
-            const results = testColors(values);
-            console.log(results);
-            actions.setSubmitting(false);
-          }}
+          validate={this.handleValidation}
+          onSubmit={this.handleSubmit}
           render={({ handleReset, isSubmitting }) => {
             // This allows us to reset the form from our custom resetForm method
             this.handleResetProxy = handleReset;
@@ -74,17 +113,24 @@ export class ColorForm extends PureComponent {
                       type="text"
                     />
                   ))}
-                  <button type="button" onClick={this.addHexInput}>
+                  <button
+                    disabled={isAddButtonDisabled}
+                    onClick={this.addHexInput}
+                    type="button"
+                  >
                     Add
                   </button>
                   <div>
-                    <button type="submit" disabled={isSubmitting}>
+                    <button
+                      disabled={isSubmitting || isSubmitButtonDisabled}
+                      type="submit"
+                    >
                       Submit
                     </button>
                   </div>
                 </Form>
               </div>
-            )
+            );
           }}
         />
         <br />
