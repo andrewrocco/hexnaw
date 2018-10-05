c5104b9a028bfd9012b2b596c306bdea30e7ce2b
Andrew Rossi <aroccorossi@gmail.com>
2018-10-05 13:24:44 -0400

    better exports, Header Logo

--- src/ui/navigation/header/Header.js
+++ src/ui/navigation/header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Box, Flex } from '@rebass/grid';
 
-import { HeaderLink } from '../header_link/HeaderLink';
+import { HeaderLink } from '../header_link';
+import { HeaderLogo } from '../header_logo';
 
 export const Header = () => (
   <Flex>
-    <Box mr="auto">Hello</Box>
+    <Box mr="auto">
+      <HeaderLogo />
+    </Box>
     <Box alignSelf="flex-end">
       <HeaderLink href="/">Hex Naw</HeaderLink>
       <HeaderLink href="/about" prefetch>About</HeaderLink>
