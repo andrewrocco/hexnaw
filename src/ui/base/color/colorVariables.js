27b733ca7d1bca42958a7f54c8752238f2a2dc87
Andy Rossi <andrewrocco@users.noreply.github.com>
2018-10-05 11:54:24 -0400

    Heading Component, Typography system, Jest (#10)
    * add fonts collection
    
    * add typography styles for header
    
    * all necessary heading variables created
    
    * forgot semicolon
    
    * add jest dependencies
    
    * add specs for heading component
    
    * add test scripts
    
    * add custom babelrc with test node env
    
    * add typography.com link
    
    * simpler import
    
    * cleanup
    
    * update readme
    
    * force coverage tests
    
    * ignore index files in coverage

--- src/ui/base/color/colorVariables.js
+++ src/ui/base/color/colorVariables.js
@@ -1,8 +1,14 @@
 import colors from './color';
 
+export const typographicColors = {
+  typeDark: colors.black,
+  typeAccent: colors.blue,
+};
+
 const colorVariables = {
   buttonPrimaryBackground: colors.blue,
   buttonPrimaryBackgroundHover: colors.blue,
+  ...typographicColors,
 };
 
 export default colorVariables;
---
+++ src/ui/base/typography/fonts.js
@@ -0,0 +1,16 @@
+import { css } from 'styled-components';
+
+const fonts = {
+  primaryMedium: css`
+    font-family: 'Gotham SSm A', 'Gotham SSm B', Helvetica, sans-serif;
+    font-style: normal;
+    font-weight: 500;
+  `,
+  primaryBold: css`
+    font-family: 'Gotham SSm A', 'Gotham SSm B', Helvetica, sans-serif;
+    font-style: normal;
+    font-weight: 700;
+  `,
+};
+
+export default fonts;
