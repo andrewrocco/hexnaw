33822601e38b492f927ed4eafce74266315549fc
Andrew Rossi <aroccorossi@gmail.com>
2018-10-05 10:50:29 -0400

    add typography styles for header

---
+++ src/ui/base/typography/typography.js
@@ -0,0 +1,24 @@
+import { css } from 'styled-components';
+
+import fonts from './fonts';
+
+const typography = {
+  headingXLarge: css`
+    ${fonts.primaryBold}
+    font-size: 30px;
+    line-height: 1.2;
+  `,
+  headingMedium: css`
+    ${fonts.primaryBold}
+    font-size: 24px;
+    line-height: 1.2;
+  `,
+  headingSmall: css`
+    ${fonts.primaryBold}
+    font-size: 14px;
+    text-transform: uppercase;
+    line-height: 1.6;
+  `,
+};
+
+export default typography;
