8996f2c3cddf63c083c8336a0a65767f7b070065
Andrew Rossi <aroccorossi@gmail.com>
2018-10-05 13:36:12 -0400

    better alignment with the grid

--- src/ui/navigation/header/Header.js
+++ src/ui/navigation/header/Header.js
@@ -5,11 +5,11 @@ import { HeaderLink } from '../header_link';
 import { HeaderLogo } from '../header_logo';
 
 export const Header = () => (
-  <Flex>
+  <Flex alignItems="center">
     <Box mr="auto">
       <HeaderLogo />
     </Box>
-    <Box alignSelf="flex-end">
+    <Box>
       <HeaderLink href="/">Hex Naw</HeaderLink>
       <HeaderLink href="/about" prefetch>About</HeaderLink>
       <HeaderLink href="https://thescenery.co" prefetch>The Scenery</HeaderLink>
