8449c01ad3578bd23ada52245607cb0a949cccfb
jonmcgill <jon.d.mcgill@gmail.com>
2018-12-07 14:37:03 -0500

    add Text component

---
+++ src/ui/typography/text/Text.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+
+import { breakpoints, colors, typography } from 'ui/base';
+
+const sizes = {
+  xsmall: typography.copyXSmall,
+  small: typography.copySmall,
+  medium: typography.copyMedium,
+  large: typography.copyLarge,
+};
+
+const getSizes = ({ size = [] }) => css`
+  ${sizes[size[0]] || sizes.small}
+  ${Object.keys(breakpoints).slice(1).map((bp, i) => (
+    `${breakpoints[bp].min} {
+      ${sizes[size[i]]}
+    }`
+  ))}
+`;
+
+export const Text = styled.span`
+  ${getSizes}
+  color: ${p => colors[p.color] || colors.black};
+`;
