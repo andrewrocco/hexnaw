04272e9a001311b292a91ba9bbea4a9d6816f60e
Andrew Rossi <aroccorossi@gmail.com>
2019-02-14 15:28:37 -0500

    app is working again with latest stuff

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet, injectGlobal } from 'styled-components';
+import { createGlobalStyle, ServerStyleSheet } from 'styled-components';
 
-// TODO: remove when using styled-components v4
-/* eslint-disable-next-line no-unused-expressions */
-injectGlobal`
+const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
@@ -16,17 +14,30 @@ injectGlobal`
 `;
 
 export default class MyDocument extends Document {
-  static async getInitialProps({ renderPage }) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
-    const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () => (
+        originalRenderPage({ enhanceApp: App => props => sheet.collectStyles(<App {...props} />) })
+      );
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: <>{initialProps.styles}{sheet.getStyleElement()}</>,
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
     return (
       <html lang="en">
         <Head>
+          <GlobalStyle />
           <link rel="shortcut icon" type="image/ico" href="/static/favicon.ico" />
           <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6366132/7734772/css/fonts.css" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
