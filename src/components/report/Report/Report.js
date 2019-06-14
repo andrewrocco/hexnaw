6474fbc5c338afce0f28c213f7d82fdc7bb1d774
Andrew Rossi <aroccorossi@gmail.com>
2019-06-14 11:02:42 -0500

    starting table work

--- src/components/report/Report/Report.js
+++ src/components/report/Report/Report.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { ReportToolbar } from './ReportToolbar';
 import { ReportTable } from './ReportTable';
 
-export const Report = ({ colorResults }) => {
-  return (
-    <div>
-      <ReportToolbar />
-      <ReportTable />
-    </div>
-  );
-}
+export const Report = ({ colorResults }) => (
+  <div>
+    <ReportToolbar />
+    {colorResults.map((result, i) => (
+      // eslint-disable-next-line react/no-array-index-key
+      <ReportTable key={`ResultTable-${i}`} result={result} />
+    ))}
+  </div>
+);
+
+Report.propTypes = {
+  colorResults: PropTypes.array,
+};
+
+Report.defaultProps = {
+  colorResults: [],
+};
--- src/components/report/Report/ReportTable.js
+++ src/components/report/Report/ReportTable.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const ReportTable = ({ results }) => {
+export const ReportTable = ({ result }) => {
+  const { hex, combinations } = result;
+
+  console.table(combinations);
+
   return (
-    <thead>
-      <tr>
-        <th>Hello</th>
-      </tr>
-    </thead>
+    <table>
+      <thead>
+        <tr>
+          <th colSpan={3}>{hex}</th>
+          <th><span>Contrast Ratio</span></th>
+          <th><span>Large Text</span></th>
+          <th><span>Small Text</span></th>
+        </tr>
+      </thead>
+      <tbody>
+        {combinations.map((combo, i) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <tr key={`${combo.hex}-${i}`}>
+            <td>{combo.hex}</td>
+            <td>{combo.contrast}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
 ReportTable.propTypes = {
-  results: PropTypes.array,
+  result: PropTypes.shape({
+    hex: PropTypes.string,
+    combinations: PropTypes.array,
+  }),
 };
