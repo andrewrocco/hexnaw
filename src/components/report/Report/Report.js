a81ae146730abfea53eff5a3bb9ad4d11b5dce3d
Andrew Rossi <aroccorossi@gmail.com>
2019-06-17 10:05:15 -0500

    wiring up event handlers

--- src/components/report/Report/Report.js
+++ src/components/report/Report/Report.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '@rebass/grid';
 
 import { ReportToolbar } from './ReportToolbar';
 import { ReportTable } from './ReportTable';
 
-export const Report = ({ colorResults }) => (
-  <div>
-    {colorResults.length >= 1 && <ReportToolbar />}
-    <Box m={4}>
-      {colorResults.map((result, i) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <ReportTable key={`ResultTable-${i}`} result={result} />
-      ))}
-    </Box>
-  </div>
-);
+export class Report extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showPassingOnly: false,
+    };
+
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle() {
+    console.log('clicked');
+  }
+
+  render() {
+    const { colorResults } = this.props;
+    const { showPassingOnly } = this.state;
+
+    return (
+      <div>
+        {colorResults.length >= 1 && (
+          <ReportToolbar
+            togglePassingColors={this.handleToggle}
+            isShowingPassing={showPassingOnly}
+          />
+        )}
+        <Box m={4}>
+          {colorResults.map((result, i) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <ReportTable key={`ResultTable-${i}`} result={result} />
+          ))}
+        </Box>
+      </div>
+    );
+  }
+}
 
 Report.propTypes = {
   colorResults: PropTypes.array,
