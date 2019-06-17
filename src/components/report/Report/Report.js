fbef8e20ed2139d55fd8f05de92a9b97a0df6a44
Andrew Rossi <aroccorossi@gmail.com>
2019-06-17 12:09:06 -0500

    switch to useState hook for Report component

--- src/components/report/Report/Report.js
+++ src/components/report/Report/Report.js
@@ -1,73 +1,55 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '@rebass/grid';
 
 import { ReportToolbar } from './ReportToolbar';
 import { ReportTable } from './ReportTable';
 
-export class Report extends PureComponent {
-  constructor(props) {
-    super(props);
+const getResultCounts = (colorResults) => {
+  let yeahCount = 0;
+  let nawCount = 0;
 
-    this.state = {
-      showPassingOnly: false,
-    };
+  colorResults.forEach(result => (
+    result.combinations.forEach((combo) => {
+      if (!combo.accessibility.aa || !combo.accessibility.aaLarge || !combo.accessibility.aaa || !combo.accessibility.aaaLarge) {
+        nawCount += 1;
+      } else {
+        yeahCount += 1;
+      }
+    })
+  ));
 
-    this.handleToggle = this.handleToggle.bind(this);
-  }
-
-  getResultMeta() {
-    const { colorResults } = this.props;
-    let yeahCount = 0;
-    let nawCount = 0;
-
-    colorResults.forEach(result => (
-      result.combinations.forEach((combo) => {
-        if (!combo.accessibility.aa || !combo.accessibility.aaLarge || !combo.accessibility.aaa || !combo.accessibility.aaaLarge) {
-          nawCount += 1;
-        } else {
-          yeahCount += 1;
-        }
-      })
-    ));
-
-    return { yeahCount, nawCount };
-  }
-
-  handleToggle() {
-    const { showPassingOnly } = this.state;
-    this.setState({ showPassingOnly: !showPassingOnly });
-  }
-
-  render() {
-    const { colorResults } = this.props;
-    const { showPassingOnly } = this.state;
-    const { yeahCount, nawCount } = this.getResultMeta();
-
-    return (
-      <div>
-        {colorResults.length >= 1 && (
-          <ReportToolbar
-            togglePassingColors={this.handleToggle}
-            isShowingPassing={showPassingOnly}
-            yeahCount={yeahCount}
-            nawCount={nawCount}
-          />
-        )}
-        <Box m={4}>
-          {colorResults.map((result, i) => (
-            <ReportTable
-              // eslint-disable-next-line react/no-array-index-key
-              key={`ResultTable-${i}`}
-              result={result}
-              isShowingPassing={showPassingOnly}
-            />
-          ))}
-        </Box>
-      </div>
-    );
-  }
+  return { yeahCount, nawCount };
 }
+
+export const Report = (props) => {
+  const [showPassingOnly, setShowPassingOnly] = useState(false);
+  const { colorResults } = props;
+  const { yeahCount, nawCount } = getResultCounts(colorResults);
+
+  return (
+    <div>
+      {colorResults.length >= 1 && (
+        <ReportToolbar
+          togglePassingColors={() => setShowPassingOnly(!showPassingOnly)}
+          isShowingPassing={showPassingOnly}
+          yeahCount={yeahCount}
+          nawCount={nawCount}
+        />
+      )}
+      <Box m={4}>
+        {colorResults.map((result, i) => (
+          <ReportTable
+            // eslint-disable-next-line react/no-array-index-key
+            key={`ResultTable-${i}`}
+            result={result}
+            isShowingPassing={showPassingOnly}
+          />
+        ))}
+      </Box>
+    </div>
+  );
+};
 
 Report.propTypes = {
   colorResults: PropTypes.array,
