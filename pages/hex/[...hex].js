2d47f344c5a99c6b1ef745a2c0457dc999459e2d
Andrew Rossi <aroccorossi@gmail.com>
2020-02-17 13:57:12 -0500

    report comes in loud and clear

--- pages/hex/[...hex].js
+++ pages/hex/[...hex].js
@@ -12,16 +12,27 @@ const Hex = () => {
   const router = useRouter();
   const { hex } = router.query;
 
+  function validateAndTest(values) {
+    if (!values) return;
+
+    // validate incoming values
+    const validHexValues = values.filter(hexID => (
+      (/^[a-fA-F0-9]{6}$|^[a-fA-F0-9]{3}$/).test(hexID)
+    ));
+
+    const results = testColors({ ...validHexValues });
+
+    // eslint-disable-next-line consistent-return
+    return results;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <AppHead />
         <Header />
         <Hero />
-        {(
-          <Report colorResults={testColors({ ...hex })} />
-        )}
-        <pre>{JSON.stringify(hex, null, 2)}</pre>
+        <Report colorResults={validateAndTest(hex)} />
         <Footer />
       </ThemeProvider>
     </>
