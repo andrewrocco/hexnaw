315151cb60aa818a60df513074a1b025baf517d1
Andrew Rossi <aroccorossi@gmail.com>
2020-02-17 13:57:53 -0500

    remove JSON stringify

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,6 @@ class Index extends Component {
             <Report colorResults={colorResults} />
           )}
           <Footer />
-          <pre>{JSON.stringify(colorResults, null, 2)}</pre>
         </Fragment>
       </ThemeProvider>
     );
