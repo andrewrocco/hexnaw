353c86e61a0d89b6876641d5dbad9cc1c3c9ec83
Andrew Rossi <aroccorossi@gmail.com>
2020-02-17 13:47:05 -0500

    testing from url

--- pages/hex/[...hex].js
+++ pages/hex/[...hex].js
@@ -2,10 +2,11 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { useRouter } from 'next/router';
 
-import { ColorForm, Report } from 'components/report';
+import { Report } from 'components/report';
 import { AppHead, Footer, Hero } from 'components/layout';
 import { Header } from 'ui/navigation';
 import { theme } from 'ui/base';
+import { testColors } from 'utils';
 
 const Hex = () => {
   const router = useRouter();
@@ -17,6 +18,9 @@ const Hex = () => {
         <AppHead />
         <Header />
         <Hero />
+        {(
+          <Report colorResults={testColors({ ...hex })} />
+        )}
         <pre>{JSON.stringify(hex, null, 2)}</pre>
         <Footer />
       </ThemeProvider>
