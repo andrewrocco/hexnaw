078e9007d02293859545cd340ca89dddbad64603
Andrew Rossi <aroccorossi@gmail.com>
2020-07-06 18:54:17 -0400

    remove link

--- src/components/layout/Footer/FooterContent.js
+++ src/components/layout/Footer/FooterContent.js
@@ -2,12 +2,6 @@ import React from 'react';
 
 export const FooterContent = () => (
   <div className="copyright">
-    <span>Quality Above All</span>
-    <a className="footer-scenery-link" href="http://thescenery.co">
-      <svg className="scenery-logo" viewBox="0 0 30 20">
-        <path d="M30.4 18.5l-6.3-11c-.4-.7-1.4-.7-1.7 0L19.3 13 12.1.5c-.4-.7-1.4-.7-1.7 0L.1 18.5c-.3.7.1 1.5.9 1.5h28.6c.7 0 1.2-.8.8-1.5z" />
-      </svg>
-    </a>
     <span>Made in Dayton, OH</span>
   </div>
 );
